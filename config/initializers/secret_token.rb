ea52718d340310e6cd43401a76951f9a41f85826
sam walton <sam@foofrancine.com>
2013-11-06 18:42:09 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stingboot::Application.config.secret_token = 'af8c7f70cdfb2cdba80babf15e50ef8419f75d58f3ecc8baf865fc10d99c17253b922e7b12df2b6807920e7de45058df9353b85e31bb2994587de36dbe9704d7'
